100a61795f85df9f482aea109f4536432818d12e
Juan Camilo Arias Arias <jucariasar@unal.edu.co>
2018-09-03 15:54:56 -0500

    Demostracion

--- periodico/articulos/models.py
+++ periodico/articulos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Reportero(models.Model):
+	nombres = models.CharField(max_length=100)
+	apellidos = models.CharField(max_length=100)
+
+	def __str__(self):
+		return
+
+class Articulo(models.Models):
+	fecha_publicacion = models.DateTimeField('Fecha de Puclicacion')
+	Prueba
+
+
